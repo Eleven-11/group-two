266dc07255f96f724e0e0a52339e676e69f42921
lovertyn <50045596+lovertyn@users.noreply.github.com>
2019-06-21 16:07:25 +0800

    notebook

---
+++ back/src/main/java/com/heeexy/example/service/impl/PostServiceImpl.java
@@ -0,0 +1,29 @@
+package com.heeexy.example.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.heeexy.example.dao.PostDao;
+import com.heeexy.example.service.PostService;
+import com.heeexy.example.util.CommonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author: lover_tyn
+ * @description: todo
+ * @date: 2019/6/18 10:50
+ * @version: 1.0
+ */
+@Service
+public class PostServiceImpl implements PostService {
+    @Autowired
+    private PostDao postDao;
+    @Override
+    public JSONObject getAllPost(JSONObject jsonObject) {
+        CommonUtil.fillPageParam(jsonObject);
+        int count = postDao.queryCountPost(jsonObject);
+        List<JSONObject> list = postDao.getAllPost(jsonObject);
+        return CommonUtil.successPage(jsonObject, list, count);
+    }
+}
