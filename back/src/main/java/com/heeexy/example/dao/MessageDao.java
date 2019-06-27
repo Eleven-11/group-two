1c522fcd580ab6f04e8798d9487174390a6db1af
SeikimaHikari <1594990334@qq.com>
2019-06-27 09:02:33 +0800

    111

---
+++ back/src/main/java/com/heeexy/example/dao/MessageDao.java
@@ -0,0 +1,96 @@
+package com.heeexy.example.dao;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @Author : 林成
+ * @Description :
+ * @Date : 2019-06-26 11:29
+ * @Version : 1.0
+ **/
+@Repository
+public interface MessageDao {
+
+    /**
+     * 获取消息列表
+     */
+    List<JSONObject> getMessage(JSONObject jsonObject);
+
+    /**
+     * 获取消息条数
+     */
+    int countMessage(JSONObject jsonObject);
+
+    /**
+     * 根据用户ID获取聊天
+     */
+    List<JSONObject> getChatByUserId(JSONObject jsonObject);
+
+    /**
+     * 获取聊天条数
+     */
+    int countChatByUserId(JSONObject jsonObject);
+
+    /**
+     * 根据用户ID获取消息
+     */
+    List<JSONObject> getMessageByUserId(JSONObject jsonObject);
+
+    /**
+     * 获取消息条数
+     */
+    int countMessageByUserId(JSONObject jsonObject);
+
+    /**
+     * 根据用户ID获取消息
+     */
+    JSONObject getMessageByMessageId(JSONObject jsonObject);
+
+    /**
+     * 获取一条消息的图片路径
+     */
+    List<JSONObject> getPictureByMessageId(int id);
+
+    /**
+     * 获取全部消息记录条数
+     */
+    int countAllMessageById(JSONObject jsonObject);
+
+    /**
+     * 获取指定状态消息记录条数
+     */
+    int countMessageById(JSONObject jsonObject);
+
+    /**
+     * 获取聊天记录编号
+     */
+    int getChat(JSONObject jsonObject);
+
+    /**
+     * 获取聊天记录条数（用于判断两用户是否存在聊天记录）
+     */
+    int countChat(JSONObject jsonObject);
+
+    /**
+     * 添加聊天记录
+     */
+    int addChat(JSONObject jsonObject);
+
+    /**
+     * 添加消息
+     */
+    int addMessage(JSONObject jsonObject);
+
+    /**
+     * 添加图片
+     */
+    int addPicture(JSONObject jsonObject);
+
+    /**
+     * 改变消息状态
+     */
+    int updateMessage(JSONObject jsonObject);
+}
---
+++ back/src/main/java/com/heeexy/example/service/impl/MessageServiceImpl.java
@@ -0,0 +1,81 @@
+package com.heeexy.example.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.heeexy.example.dao.MessageDao;
+import com.heeexy.example.service.MessageService;
+import com.heeexy.example.util.CommonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Author : 林成
+ * @Description :
+ * @Date : 2019-06-26 16:47
+ * @Version : 1.0
+ **/
+@Service
+public class MessageServiceImpl implements MessageService {
+    @Autowired
+    private MessageDao messageDao;
+
+    @Override
+    public JSONObject getMessage(JSONObject jsonObject) {
+        CommonUtil.fillPageParam(jsonObject);
+        int count = messageDao.countMessage(jsonObject);
+        List<JSONObject> list = messageDao.getMessage(jsonObject);
+        return CommonUtil.successPage(jsonObject, list, count);
+    }
+
+    @Override
+    public JSONObject getChatByUserId(JSONObject jsonObject) {
+        CommonUtil.fillPageParam(jsonObject);
+        int count = messageDao.countChatByUserId(jsonObject);
+        List<JSONObject> list = messageDao.getChatByUserId(jsonObject);
+        return CommonUtil.successPage(jsonObject, list, count);
+    }
+
+    @Override
+    public JSONObject getMessageByUserId(JSONObject jsonObject) {
+        CommonUtil.fillPageParam(jsonObject);
+        int count = messageDao.countMessageByUserId(jsonObject);
+        List<JSONObject> list = messageDao.getMessageByUserId(jsonObject);
+        return CommonUtil.successPage(jsonObject, list, count);
+    }
+
+    @Override
+    public JSONObject getMessageByMessageId(JSONObject jsonObject) {
+        JSONObject message = messageDao.getMessageByMessageId(jsonObject);
+        return CommonUtil.successJson(message);
+    }
+
+    @Override
+    public JSONObject getPictureByMessageId(int id) {
+        List<JSONObject> list = messageDao.getPictureByMessageId(id);
+        return CommonUtil.successJson(list);
+    }
+
+    @Override
+    public JSONObject addMessage(JSONObject jsonObject) {
+        if(messageDao.countChat(jsonObject)==0){
+            messageDao.addChat(jsonObject);
+        }
+        int cid = messageDao.getChat(jsonObject);
+        jsonObject.put("cid", cid);
+        messageDao.addMessage(jsonObject);
+        return CommonUtil.successJson();
+    }
+
+    @Override
+    public JSONObject addPicture(JSONObject jsonObject) {
+        messageDao.addPicture(jsonObject);
+        return CommonUtil.successJson();
+    }
+
+    @Override
+    public JSONObject updateMessage(JSONObject jsonObject) {
+        messageDao.updateMessage(jsonObject);
+        return CommonUtil.successJson();
+    }
+}
