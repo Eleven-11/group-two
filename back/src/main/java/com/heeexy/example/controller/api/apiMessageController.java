9ac5738f9883b2f3ed678084cd5fd4fe607dba3c
SeikimaHikari <1594990334@qq.com>
2019-07-09 09:42:09 +0800

    7/5 commit

--- back/src/main/java/com/heeexy/example/controller/api/apiMessageController.java
+++ back/src/main/java/com/heeexy/example/controller/api/apiMessageController.java
@@ -2,10 +2,9 @@ package com.heeexy.example.controller.api;
 
 import com.alibaba.fastjson.JSONObject;
 import com.heeexy.example.service.MessageService;
+import com.heeexy.example.util.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author : 林成
@@ -32,5 +31,17 @@ public class apiMessageController {
         jsonObject.put("uid", uid);
         jsonObject.put("cid", cid);
         return messageService.getMessageByChatId(jsonObject);
+    }
+
+    @PostMapping("addMessage")
+    public JSONObject addMessage(@RequestBody JSONObject jsonObject){
+        CommonUtil.hasAllRequired(jsonObject, "cid,fuid,tuid");
+        return messageService.addMessage(jsonObject);
+    }
+
+    @PostMapping("updateMessage")
+    public JSONObject updateMessage(@RequestBody JSONObject jsonObject){
+        CommonUtil.hasAllRequired(jsonObject, "mid,status");
+        return messageService.updateMessage(jsonObject);
     }
 }
