19e763560543c8f302e248d3d3d5df93162d41e0
lovertyn <50045596+lovertyn@users.noreply.github.com>
2019-07-09 16:08:57 +0800

    finish1

--- back/src/main/java/com/heeexy/example/controller/api/PostExhibitController.java
+++ back/src/main/java/com/heeexy/example/controller/api/PostExhibitController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @version: 1.0
  */
 @RestController
-@RequestMapping("/information")
+@RequestMapping("/api/information")
 public class PostExhibitController {
 
     @Autowired
